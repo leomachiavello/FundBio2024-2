bda85e00f64d0e1236bc653a711a4a961591f540
leomachiavello <leomachiavello@gmail.com>
2024-10-28 11:39:38 -0500

    Añado código de prueba arduino

---
+++ Software/codigofunbio.ino
@@ -0,0 +1,35 @@
+const int pinSensor = A0;
+const int pinBuzzer = A1;
+
+float valorSensor = 0;
+float voltaje = 0;
+float voltajeMax = 3.0;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(pinBuzzer, OUTPUT);
+
+}
+
+void loop() {
+
+  int valorSensor = analogRead(pinSensor);
+  float voltaje = valorSensor * (5.0/1023.0);
+  
+  Serial.print("Valor del sensor de presión: ");
+  Serial.print(valorSensor);
+  Serial.print(" - Voltaje: ");
+  Serial.println(voltaje, 3);
+
+  if (voltaje >= voltajeMax){
+    tone(buzzerPin, 1000);
+  } else {
+    noTone(buzzerPin);
+  }
+
+  delay(1000);
+
+}
+
+
