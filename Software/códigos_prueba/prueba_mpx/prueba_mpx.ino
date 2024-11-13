4eee3d2e5f135836c6a33daf8bfd3d9442a44ac1
leomachiavello <leomachiavello@gmail.com>
2024-11-12 20:24:04 -0500

    Actualización códigos

--- Software/códigos_prueba/prueba_mpx/prueba_mpx.ino
+++ Software/códigos_prueba/prueba_mpx/prueba_mpx.ino
@@ -1,25 +1,32 @@
-const int pinSensor = A1;
+const int pinSensor1 = A0;
+const int pinSensor2 = A1;
 
-float valorSensor = 0;
-float voltaje = 0;
-float voltajeMax = 3.0;
+float valorSensor1 = 0;
+float valorSensor2 = 0;
 
 void setup() {
-
+  
   Serial.begin(9600);
-
+ 
 }
 
 void loop() {
 
-  int valorSensor = analogRead(pinSensor);
-  float voltaje = valorSensor * (5.0/1023.0);
-  float voltajeAbs = fabs(voltaje-1.4);
-  
-  Serial.print("Valor del sensor de presión: ");
-  Serial.print(valorSensor);
-  Serial.print(" - Voltaje: ");
-  Serial.println(voltajeAbs, 3);
+  int valorSensor1 = analogRead(pinSensor1);
+  int valorSensor2 = analogRead(pinSensor2);
+
+  Serial.print("Diferencia de presión: ");
+  Serial.println(fabs(valorSensor1-valorSensor2));
+
+  float voltaje = fabs(valorSensor1-valorSensor2) * (5.0/1023.0);
+  Serial.print("Voltaje: ");
+  Serial.print(voltaje * 1000, 3);
+  Serial.println(" mV");
+
+  float presion = voltaje / 0.009;
+  Serial.print("Presión: ");
+  Serial.print(presion);
+  Serial.println(" kPa");
 
   delay(1000);
 
---
+++ Software/códigos_prueba/prueba_mpx_ina/prueba_mpx_ina.ino
@@ -0,0 +1,26 @@
+const int pinSensor = A1;
+
+float valorSensor = 0;
+float voltaje = 0;
+float voltajeMax = 3.0;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  int valorSensor = analogRead(pinSensor);
+  float voltaje = valorSensor * (5.0/1023.0);
+  float voltajeAbs = fabs(voltaje);
+  
+  Serial.print("Valor del sensor de presión: ");
+  Serial.print(valorSensor);
+  Serial.print(" - Voltaje: ");
+  Serial.println(voltajeAbs, 3);
+
+  delay(1000);
+
+}
